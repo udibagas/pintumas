6bbe14f6906c4e59aa034272ab2fb041e3f66f6b
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-06-12 17:04:17 +0700

    simplify

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -3,28 +3,24 @@
 import React, { useState } from 'react';
 import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Avatar, Dropdown, Layout, Modal, Space, theme, Typography } from 'antd';
+import { Avatar, Button, Dropdown, Layout, Modal } from 'antd';
 import NavMenu from '@/components/nav-menu';
 import client from '@/lib/api-client';
 import { redirect } from 'next/navigation';
 
-const { Text } = Typography;
 const { Header, Content, Sider } = Layout;
 
 export default function MainLayout({ children }: React.PropsWithChildren) {
   const [collapsed, setCollapsed] = useState(false);
 
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   function logout() {
     Modal.confirm({
-      title: 'Confirmation',
-      content: 'Are you sure you want to logout?',
+      title: 'Konfrimasi',
+      content: 'Anda yakin akan keluar?',
+      centered: true,
       icon: <LogoutOutlined />,
-      okText: 'Yes',
-      cancelText: 'No',
+      okText: 'Ya',
+      cancelText: 'Tidak',
       onOk: () => {
         client.post('/auth/logout').then(() => {
           redirect('/login');
@@ -67,10 +63,10 @@ export default function MainLayout({ children }: React.PropsWithChildren) {
         <Header style={{ padding: '0 20px', background: '#fff', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <div style={{ fontSize: 24, color: '#0C74B6', fontWeight: 'bold' }}>PINTUMAS</div>
           <Dropdown menu={{ items: menuItems }} placement="bottom" arrow>
-            <Space>
-              <Avatar style={{ backgroundColor: '#87d068' }} size={25} icon={<UserOutlined />} />
-              <Text strong>Selamat Datang, {`Admin User`}!</Text>
-            </Space>
+            <Button type='text'>
+              <Avatar size='small' icon={<UserOutlined />} />
+              <span>{`Admin User`}</span>
+            </Button>
           </Dropdown>
         </Header>
 
