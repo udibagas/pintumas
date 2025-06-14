517f38e1a922bcd9c6b8fdb75936aed5ba56ad30
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-06-14 10:00:44 +0700

    Department & Category

--- src/app/(admin)/admin/posts/PostForm.tsx
+++ src/app/(admin)/admin/posts/PostForm.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Button, Col, Form, Input, message, Row, Switch } from "antd";
-import { Post } from "@prisma/client";
-import { SaveOutlined, LoadingOutlined } from "@ant-design/icons";
-import { ServerErrorResponse } from "@/types";
-import { createItem, updateItem } from "@/lib/api-client";
+import { Button, Col, Form, Image, Input, message, Row, Select, Switch } from "antd";
+import { Category, Department, Post } from "@prisma/client";
+import { SaveOutlined } from "@ant-design/icons";
+import { PaginatedData, ServerErrorResponse } from "@/types";
+import { createItem, getItems, updateItem } from "@/lib/api-client";
 import { useRouter } from "next/navigation";
 
 export default function PostForm({ values }: { values: Post }) {
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [form] = Form.useForm<Post>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [errors, setErrors] = useState<Record<string, string[]>>({});
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [departments, setDepartments] = useState<Department[]>([]);
   const router = useRouter();
 
   const onOk = async (values: Post) => {
@@ -41,6 +43,14 @@ export default function PostForm({ values }: { values: Post }) {
   };
 
   useEffect(() => {
+    getItems<PaginatedData<Category>>('/api/categories', { pageSize: 1000 })
+      .then(data => setCategories(data.rows));
+
+    getItems<PaginatedData<Department>>('/api/departments', { pageSize: 1000 })
+      .then(data => setDepartments(data.rows));
+  }, [])
+
+  useEffect(() => {
     if (values) {
       form.setFieldsValue(values);
     } else {
@@ -57,14 +67,13 @@ export default function PostForm({ values }: { values: Post }) {
       requiredMark={false}
       colon={false}
       layout="vertical"
-      size="large"
     >
       <Form.Item name="id" hidden>
         <Input />
       </Form.Item>
 
-      <Row gutter={20}>
-        <Col span={12}>
+      <Row gutter={40}>
+        <Col span={16}>
           <Form.Item
             label="Judul"
             name="title"
@@ -80,37 +89,79 @@ export default function PostForm({ values }: { values: Post }) {
             validateStatus={errors.content ? "error" : ""}
             help={errors.content?.join(", ")}
           >
-            <Input.TextArea placeholder="Tulis postingan Anda di sini" rows={10} />
+            <Input.TextArea placeholder="Tulis postingan Anda di sini" rows={20} />
           </Form.Item>
         </Col>
 
-        <Col span={12}>
-          <Form.Item
-            label="Featured"
-            name="featured"
-            validateStatus={errors.featured ? "error" : ""}
-            help={errors.featured?.join(", ")}
-          >
-            <Switch defaultChecked={false} />
-          </Form.Item>
+        <Col span={8}>
+          <Image src="https://picsum.photos/600/400" alt="Foto" preview={false} className="rounded-lg mb-8" />
+          <Row gutter={40}>
+            <Col span={12}>
+              <Form.Item
+                label="Kategori"
+                name="categoryId"
+                validateStatus={errors.categoryId ? "error" : ""}
+                help={errors.categoryId?.join(", ")}
+              >
+                <Select placeholder="Pilih kategori">
+                  {
+                    categories.map((category) => (
+                      <Select.Option key={category.id} value={category.id}>
+                        {category.name}
+                      </Select.Option>
+                    ))
+                  }
+                </Select>
+              </Form.Item>
 
-          <Form.Item
-            label="Publish"
-            name="published"
-            validateStatus={errors.published ? "error" : ""}
-            help={errors.published?.join(", ")}
-          >
-            <Switch defaultChecked={false} />
+              <Form.Item
+                label="Departemen"
+                name="departmentId"
+                validateStatus={errors.departmentId ? "error" : ""}
+                help={errors.departmentId?.join(", ")}
+              >
+                <Select placeholder="Pilih Departemen">
+                  {
+                    departments.map((department) => (
+                      <Select.Option key={department.id} value={department.name}>
+                        {department.name}
+                      </Select.Option>
+                    ))
+                  }
+                </Select>
+              </Form.Item>
+            </Col>
+
+            <Col span={12}>
+              <Form.Item
+                label="Unggulan"
+                name="featured"
+                validateStatus={errors.featured ? "error" : ""}
+                help={errors.featured?.join(", ")}
+              >
+                <Switch defaultChecked={false} />
+              </Form.Item>
+
+              <Form.Item
+                label="Terbitkan"
+                name="published"
+                validateStatus={errors.published ? "error" : ""}
+                help={errors.published?.join(", ")}
+              >
+                <Switch defaultChecked={false} />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Form.Item>
+            <Button color="default" variant="solid" htmlType="submit" className="w-full" loading={isSubmitting}>
+              {!isSubmitting && <SaveOutlined />}
+              {isSubmitting ? 'Menyimpan data...' : 'Simpan'}
+            </Button>
           </Form.Item>
         </Col>
       </Row>
 
-      <Form.Item>
-        <Button color="default" variant="solid" htmlType="submit" className="mr-4 w-40">
-          {isSubmitting ? <LoadingOutlined /> : <SaveOutlined />}
-          {isSubmitting ? 'Menyimpan data...' : 'Simpan'}
-        </Button>
-      </Form.Item>
     </Form>
   );
 };
